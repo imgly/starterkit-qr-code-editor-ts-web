7a67fb4982b0041c8ecc720efe2915dda1c45371
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-09-09 20:05:32 +0200

    Update starterkit for v1.82.0-rc.2

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import CreativeEditorSDK from '@cesdk/cesdk-js';
 import { initQRCodeEditor } from './imgly';
 import { resolveAssetPath } from './imgly/resolveAssetPath';
 
+
 // ============================================================================
 // Configuration
 // ============================================================================
